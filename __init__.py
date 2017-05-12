6eb8de9fe4fbb5c87430739859e366f9463f0c86
Andrew Chael <achael@cfa.harvard.edu>
2017-05-12 18:56:20 -0400

    created reorganized library

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-.. module:: eht-imaging
-    :platform: Unix
-    :synopsis: EHT Imaging Utilities
-
-.. moduleauthor:: Andrew Chael (achael@cfa.harvard.edu)
-
-"""
-
--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 import os
 from setuptools import setup
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 if __name__ == "__main__":
-    import vlbi_imaging_utils
-    import maxen
     setup(
-        name="PolMEM",
+        name="ehtim",
         version = "0.0.1",
-        packages = ["vlbi_imaging_utils","maxen"],
+
         author = "Andrew Chael",
         author_email = "achael@cfa.harvard.edu",
-        description = ("Python classes to load, simulate, and manipulate VLBI images and datasets and a collection of Maximum Entropy imaging functions in total intensity and polarization."),
+        description = ("Python code to load, simulate, and manipulate VLBI datasets" +
+                        "and a collection of imaging functions in total intensity and polarization."),
         license = "MIT",
-        keywords = "imaging astronomy EHT polarimetry"
+        keywords = "imaging astronomy EHT polarimetry",
+        url = "https://github.com/achael/eht-imaging",
+        packages = ["ehtim","ehtim.observing","ehtim.plotting","ehtim.io","ehtim.imaging","ehtim.calib"],
+        long_description=read('README.md'),
     )
