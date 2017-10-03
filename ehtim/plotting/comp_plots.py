b6af83409141b3e9920912c91d5667bf87478826
Katherine Bouman <klbouman@Katherines-MacBook-Pro-3.local>
2017-10-03 18:26:20 -0400

    added vtypes to plotting

--- ehtim/plotting/comp_plots.py
+++ ehtim/plotting/comp_plots.py
@@ -15,38 +15,38 @@ COLORLIST = ['b','m','g','c','y','k','r']
 ##################################################################################################
 def plotall_obs_compare(obslist, field1, field2, rangex=False, rangey=False, conj=False, show=True, clist=COLORLIST):
     """Plot data from multiple observations on the same axes.
-    """
-
+        """
+    
     try: len(obslist)
     except TypeError: obslist = [obslist]
-
+    
     if len(obslist) > len(clist):
         Exception("More observations than colors -- Add more colors to clist!")
-
+    
     axis = False
     for i in range(len(obslist)):
         obs = obslist[i]
         axis = obs.plotall(field1, field2, rangex=rangex, rangey=rangey, conj=conj, show=False, axis=axis, color=clist[i%len(clist)])
-
+    
     if show:
         plt.show(block=False)
     return axis
 
 def plot_bl_obs_compare(obslist,  site1, site2, field, rangex=False, rangey=False, show=True, clist=COLORLIST):
     """Plot data from multiple observations vs time on a single baseline on the same axes.
-    """
-
+        """
+    
     try: len(obslist)
     except TypeError: obslist = [obslist]
-
+    
     if len(obslist) > len(clist):
         Exception("More observations than colors -- Add more colors to clist!")
-
+    
     axis = False
     for i in range(len(obslist)):
         obs = obslist[i]
         axis = obs.plot_bl(site1, site2, field, rangex=rangex, rangey=rangey, show=False, axis=axis, color=clist[i%len(clist)])
-
+    
     if show:
         plt.show(block=False)
     return axis
@@ -54,19 +54,19 @@ def plot_bl_obs_compare(obslist,  site1, site2, field, rangex=False, rangey=Fals
 
 def plot_cphase_obs_compare(obslist,  site1, site2, site3, vtype='vis', rangex=False, rangey=False, show=True, clist=COLORLIST):
     """Plot closure phase on a triangle vs time from multiple observations on the same axes.
-    """
-
+        """
+    
     try: len(obslist)
     except TypeError: obslist = [obslist]
-
+    
     if len(obslist) > len(clist):
         Exception("More observations than colors -- Add more colors to clist!")
-
+    
     axis = False
     for i in range(len(obslist)):
         obs = obslist[i]
         axis = obs.plot_cphase(site1, site2, site3, vtype=vtype, rangex=rangex, rangey=rangey, show=False, axis=axis, color=clist[i%len(clist)])
-
+    
     if show:
         plt.show(block=False)
     return axis
@@ -74,19 +74,19 @@ def plot_cphase_obs_compare(obslist,  site1, site2, site3, vtype='vis', rangex=F
 
 def plot_camp_obs_compare(obslist,  site1, site2, site3, site4, vtype='vis', rangex=False, rangey=False, show=True, clist=COLORLIST):
     """Plot closure amplitude on a triangle vs time from multiple observations on the same axes.
-    """
-
+        """
+    
     try: len(obslist)
     except TypeError: obslist = [obslist]
-
+    
     if len(obslist) > len(clist):
         Exception("More observations than colors -- Add more colors to clist!")
-
+    
     axis = False
     for i in range(len(obslist)):
         obs = obslist[i]
         axis = obs.plot_camp(site1, site2, site3, site4, vtype=vtype, rangex=rangex, rangey=rangey, show=False, axis=axis, color=clist[i%len(clist)])
-
+    
     if show:
         plt.show(block=False)
     return axis
@@ -96,49 +96,49 @@ def plot_camp_obs_compare(obslist,  site1, site2, site3, site4, vtype='vis', ran
 ##################################################################################################
 def plotall_obs_im_compare(obslist, image, field1, field2, sgrscat=False, rangex=False, rangey=False, conj=False, show=True, clist=COLORLIST):
     """Plot data from observations compared to ground truth from an image on the same axes.
-    """
-
+        """
+    
     try: len(obslist)
     except TypeError: obslist = [obslist]
-
+    
     for i in range(len(obslist)):
         obstrue = image.observe_same(obslist[i], sgrscat=sgrscat, add_th_noise=False)
         obstrue.data['sigma'] *= 0
         obslist.append(obstrue)
-
+    
     if len(obslist) > len(clist):
         Exception("More observations than colors -- Add more colors to clist!")
-
+    
     axis = False
     for i in range(len(obslist)):
         obs = obslist[i]
         axis = obs.plotall(field1, field2, rangex=rangex, rangey=rangey, conj=conj, show=False, axis=axis, color=clist[i%len(clist)])
-
+    
     if show:
         plt.show(block=False)
     return axis
 
 def plot_bl_obs_im_compare(obslist, image, site1, site2, field, sgrscat=False,  rangex=False, rangey=False, show=True, clist=COLORLIST):
     """Plot data vs time on a single baseline compared to ground truth from an image on the same axes.
-    """
-
+        """
+    
     try: len(obslist)
-
+    
     except TypeError: obslist = [obslist]
-
+    
     for i in range(len(obslist)):
         obstrue = image.observe_same(obslist[i], sgrscat=sgrscat, add_th_noise=False)
         obstrue.data['sigma'] *= 0
         obslist.append(obstrue)
-
+    
     if len(obslist) > len(clist):
         Exception("More observations than colors -- Add more colors to clist!")
-
+    
     axis = False
     for i in range(len(obslist)):
         obs = obslist[i]
         axis = obs.plot_bl(site1, site2, field, rangex=rangex, rangey=rangey, sgrscat=False, show=False, axis=axis, color=clist[i%len(clist)])
-
+    
     if show:
         plt.show(block=False)
     return axis
@@ -146,24 +146,24 @@ def plot_bl_obs_im_compare(obslist, image, site1, site2, field, sgrscat=False,  
 
 def plot_cphase_obs_im_compare(obslist, image, site1, site2, site3, vtype='vis', sgrscat=False, rangex=False, rangey=False, show=True, clist=COLORLIST):
     """Plot closure phase on a triangle compared to ground truth from an image on the same axes.
-    """
-
+        """
+    
     try: len(obslist)
     except TypeError: obslist = [obslist]
-
+    
     for i in range(len(obslist)):
         obstrue = image.observe_same(obslist[i], sgrscat=sgrscat,add_th_noise=False)
         obstrue.data['sigma'] *= 0
         obslist.append(obstrue)
-
+    
     if len(obslist) > len(clist):
         Exception("More observations than colors -- Add more colors to clist!")
-
+    
     axis = False
     for i in range(len(obslist)):
         obs = obslist[i]
         axis = obs.plot_cphase(site1, site2, site3, vtype=vtype, rangex=rangex, rangey=rangey, show=False, axis=axis, color=clist[i%len(clist)])
-
+    
     if show:
         plt.show(block=False)
     return axis
@@ -171,24 +171,24 @@ def plot_cphase_obs_im_compare(obslist, image, site1, site2, site3, vtype='vis',
 
 def plot_camp_obs_im_compare(obslist, image, site1, site2, site3, site4, vtype='vis', sgrscat=False, rangex=False, rangey=False, show=True, clist=COLORLIST):
     """Plot closure amplitude on a quadrangle compared to ground truth from an image on the same axes.
-    """
-
+        """
+    
     try: len(obslist)
     except TypeError: obslist = [obslist]
-
+    
     for i in range(len(obslist)):
         obstrue = image.observe_same(obslist[i], sgrscat=sgrscat, add_th_noise=False)
         obstrue.data['sigma'] *= 0
         obslist.append(obstrue)
-
+    
     if len(obslist) > len(clist):
         Exception("More observations than colors -- Add more colors to clist!")
-
+    
     axis = False
     for i in range(len(obslist)):
         obs = obslist[i]
         axis = obs.plot_camp(site1, site2, site3, site4, vtype=vtype, rangex=rangex, rangey=rangey, show=False, axis=axis, color=clist[i%len(clist)])
-
+    
     if show:
         plt.show(block=False)
     return axis
