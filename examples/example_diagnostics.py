c8d76be10c1aaee83207269eee7455d27917c01d
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-10-10 21:18:11 -0400

    Use test_2 in the imaging challenging result gallery

--- examples/example_diagnostics.py
+++ examples/example_diagnostics.py
@@ -4,6 +4,7 @@ import ehtim.diagnostics as ed
 
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage import io
 
 def gauss(x0, sx):
     n = 128
@@ -14,13 +15,17 @@ def gauss(x0, sx):
 def gauss2(x0, y0, sx, sy):
     return np.sqrt(np.outer(gauss(x0, sx), gauss(y0, sy)))
 
-img1 = gauss2(0.0,0.0,0.1,0.1)
-img2 = 0.5 * (gauss2(0.0,-0.1,0.1,0.1) + gauss2(0.0,0.1,0.1,0.1))
-img3 = 0.5 * (gauss2(0.0,-0.2,0.1,0.1) + gauss2(0.0,0.2,0.1,0.1))
-imgs = [img1, img2, img3]
-labels = ['One Gaussian', 'Two nearby Gaussians', 'Two far-away Gaussians']
 
-n = 32
+urltemplate = "http://vlbiimaging.csail.mit.edu/static/testData/uploads/{}/pngimages/test_02.png"
+submissions = ["2617818", "7039776", "8649732", "6288084", "5869777", "6356229"]
+labels      = ["CHRIP - Natural", "SQUEEZE - Total Variation", "BSMEM", "Bispectrum MEM L-BFGS",
+               "Bispectral MEM L-BFGS Total Variation", "CHIRP - Celestial"]
+
+urls = [urltemplate.format(s) for s in submissions]
+imgs = [io.imread(url) for url in urls]
+imgs = [img / np.sum(img.astype(float)) for img in imgs]
+
+n = 8
 x = np.arange(n * n)
 oneds, ref = ed.onedimize(imgs, n=n)
 
