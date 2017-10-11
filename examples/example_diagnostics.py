7d108b3eff84736360f9b11a564e56e578ea03c1
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-10-11 01:08:29 -0400

    Having multiple plots in a row makes them easier to read

--- examples/example_diagnostics.py
+++ examples/example_diagnostics.py
@@ -18,30 +18,27 @@ def gauss2(x0, y0, sx, sy):
 
 urltemplate = "http://vlbiimaging.csail.mit.edu/static/testData/uploads/{}/pngimages/test_02.png"
 submissions = ["2617818", "7039776", "8649732", "6288084", "5869777", "6356229"]
-labels      = ["CHRIP - Natural", "SQUEEZE - Total Variation", "BSMEM", "Bispectrum MEM L-BFGS",
-               "Bispectral MEM L-BFGS Total Variation", "CHIRP - Celestial"]
+labels      = ["CHRIP\nNatural", "SQUEEZE\nTotal Variation", "BSMEM", "Bispectrum MEM\nL-BFGS",
+               "Bispectral MEM\nL-BFGS Total Variation", "CHIRP\nCelestial"]
 
 urls = [urltemplate.format(s) for s in submissions]
 imgs = [io.imread(url) for url in urls]
 imgs = [img / np.sum(img.astype(float)) for img in imgs]
 
 n = 8
-x = np.arange(n * n)
+x = np.arange(n * n) + 1
 oneds, ref = ed.onedimize(imgs, n=n)
 
-f, axs = plt.subplots(2, sharex=True)
+f, axs = plt.subplots(1, len(submissions), sharex=True, sharey=True)
 
-axs[0].step(x, ref, linewidth=3, color='k', label='Mean')
 for i, oned in enumerate(oneds):
-    axs[0].step(x, oned, alpha=0.5, label=labels[i])
-    axs[0].legend()
-    axs[0].set_ylabel('Intensity')
+    axs[i].step(np.log(x), np.log(ref),  linewidth=3, color='k')
+    axs[i].step(np.log(x), np.log(oned), alpha=0.5)
+    axs[i].set_title(labels[i])
+    axs[i].set_xlabel('Sorting index')
+    if i == 0:
+        axs[i].set_ylabel('ln(Intensity)')
 
-axs[1].step(x, np.log(ref), linewidth=3, color='k', label='Mean')
-for i, oned in enumerate(oneds):
-    axs[1].step(x, np.log(oned), alpha=0.5, label=labels[i])
-    axs[1].legend()
-    axs[1].set_xlabel('Sorting index')
-axs[1].set_ylabel('ln(Intensity)')
-
+axs[0].set_ylim(-8,-2)
+f.subplots_adjust(hspace=0,wspace=0)
 plt.show()
