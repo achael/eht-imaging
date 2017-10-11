2285f71a36b9c5709c77d81cdf1884ab9a997938
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-10-10 20:11:56 -0400

    Down sampling image by first padding the image

--- ehtim/diagnostics.py
+++ ehtim/diagnostics.py
@@ -1,3 +1,8 @@
+from __future__ import division
+from __future__ import print_function
+from builtins import range
+from builtins import object
+
 import numpy as np
 
 def resample(y, n=16):
@@ -16,3 +21,23 @@ def resample(y, n=16):
     csum[nnew] = sum(y)
 
     return np.diff(csum)
+
+def resample2(img, n=16):
+    """Resampling an image
+
+    For simplicity, we will just pad each side of an image to an
+    integer multiple of n and then down sample but summing up the
+    intensity of the subcells.  This is not perfect but is good enough
+    for a proof of concept.
+    """
+    nx = img.shape[0]
+    ny = img.shape[1]
+    mx = (nx - 1)//n + 1
+    my = (ny - 1)//n + 1
+    Nx = mx * n
+    Ny = my * n
+    px = (Nx - nx)//2
+    py = (Ny - ny)//2
+
+    img = np.pad(img, ((px, Nx-nx-px), (py, Ny-ny-py)), 'constant')
+    return img.reshape(n, mx, n, my).sum(axis=(1,3))
