ce60541b5e656204553dc7ee0b8fc42bf029a42c
michaeldjohnson <michaeltdh@gmail.com>
2017-12-14 11:50:11 -0500

    Added master caltable tracking. Fixed bug when sites are not present for network calibration.

--- scripts/calibrate.py
+++ scripts/calibrate.py
@@ -21,10 +21,13 @@ def pick(obs, req_sites):
     mask     = [req_sites.issubset(set(tlist['t1']).union(tlist['t2']))
                 for tlist in tlists]
     out      = obs.copy()
+    if len(tlists[mask]) == 0:
+        return []
+
     out.data = np.concatenate(tlists[mask])
     return out
 
-def multical(obs, sites, n=3, amp0=8.0, gain_tol=0.1):
+def multical(obs, sites, master_caltab, n=3, amp0=8.0, gain_tol=0.1, only_amp=True):
     """
     Apply network_cal() multiple times
     """
@@ -36,6 +39,9 @@ def multical(obs, sites, n=3, amp0=8.0, gain_tol=0.1):
               'gain_tol':gain_tol,
               'processes':0,
               'caltable':True}
+    # If the specified sites aren't present, skip the calibration
+    if len(pick(obs,sites).data) == 0:
+        return obs
 
     for i in range(n):
         # Self calibrate the amplitudes
@@ -45,6 +51,13 @@ def multical(obs, sites, n=3, amp0=8.0, gain_tol=0.1):
         obs = caltab.applycal(obs, interp='nearest', extrapolate=True)
         caltab.save_txt(obs, datadir=datadir)
         obs_cal_avg.save_uvfits(datadir+'/'+args.output)
+        if master_caltab == None:
+            master_caltab = caltab
+        else:
+            master_caltab.merge(caltab)
+
+        if only_amp: 
+            continue
 
         # Self calibrate the phases
         datadir = '{}-{}-phase'.format(stepname, i)
@@ -53,8 +66,12 @@ def multical(obs, sites, n=3, amp0=8.0, gain_tol=0.1):
         obs = caltab.applycal(obs, interp='nearest', extrapolate=True)
         caltab.save_txt(obs, datadir=datadir)
         obs_cal_avg.save_uvfits(datadir+'/'+args.output)
+        if master_caltab == None:
+            master_caltab = caltab
+        else:
+            master_caltab.merge(caltab)
 
-    return obs
+    return [obs, master_caltab]
 
 # Tack to experiment number mapper (Unused, just for record)
 expt = {'D':3597,
@@ -66,7 +83,7 @@ expt = {'D':3597,
 # Argument parsing
 parser = argparse.ArgumentParser(description="Perform network calibration")
 parser.add_argument('input',                                    help="input uvfits file")
-parser.add_argument('caltab',                                   help="caltable directory")
+parser.add_argument('-c', '--caldir', default=None,             help="caltable directory")
 parser.add_argument('-o', '--output', default=None,             help="output file")
 parser.add_argument('-P', '--prune',  default=1,    type=int,   help="pruning factor")
 parser.add_argument('-z', '--ampzbl', default=7.0,  type=float, help="amplitude at zero-baseline")
@@ -78,7 +95,7 @@ if args.output is None:
     args.output = os.path.basename(args.input[:-13])+args.pol+args.pol+'+netcal.uvfits'
 print("Parameters:")
 print("    input: ", args.input)
-print("    caltab:", args.caltab)
+print("    caltab directory:", args.caldir)
 print("    output:", args.output)
 print("    prune: ", args.prune)
 print("    ampzbl:", args.ampzbl)
@@ -87,35 +104,48 @@ print("    pol:   ", args.pol)
 
 # Load uvfits file
 obs = eh.obsdata.load_uvfits(args.input, force_singlepol=args.pol)
+print("Flagging the SMA Reference Antenna...")
 obs = obs.flag_sites(["SR"])
+print("Flagging points with anomalous snr...")
 obs = obs.flag_anomalous('snr', robust_nsigma_cut=3.0)
 
-# A-priori calibrate by applying the caltable
-caltab  = eh.caltable.load_caltable(obs, args.caltab)
-obs_cal = caltab.applycal(obs, interp='nearest', extrapolate=True, force_singlepol=args.pol)
+# Optional: A-priori calibrate by applying the caltable
+if args.caldir != None:
+    print("Loading the a priori calibration table...")
+    caltab  = eh.caltable.load_caltable(obs, args.caldir)
+    obs_cal = caltab.applycal(obs, interp='nearest', extrapolate=True, force_singlepol=args.pol)
+else:
+    obs_cal = obs.copy()
 
-# Compute averages
+# Coherently average the input data with a specified coherence time
 obs_cal_avg = obs_cal.avg_coherent(args.tavg)
+# Flag for anomalous snr in the averaged data
+obs_cal_avg = obs_cal_avg.flag_anomalous('snr', robust_nsigma_cut=3.0)
+# Save the averaged data
 obs_cal_avg.save_uvfits(os.path.basename(args.input[:-13])+args.pol+args.pol+'+avg.uvfits')
 
 # Speed up testing
 if args.prune > 1:
     obs_cal_avg.data = np.concatenate(obs_cal_avg.tlist()[::args.prune])
 
-# First get the ALMA and APEX calibration right -- allow huge gain_tol
+# Initialize the master caltable
+master_caltab = None
+
+# First get the ALMA and APEX calibration right -- allow modest gain_tol
 stepname = 'step1'
 sites = {'AA','AP'}
-obs_cal_avg = multical(obs_cal_avg, sites, n=5, amp0=args.ampzbl, gain_tol=10.0)
+[obs_cal_avg, master_caltab] = multical(obs_cal_avg, sites, master_caltab, n=2, amp0=args.ampzbl, gain_tol=0.3)
 
 # Next get the SMA and JCMT calibration right -- allow modest gain_tol
 stepname = 'step2'
 sites = {'SM','JC'}
-obs_cal_avg = multical(obs_cal_avg, sites, n=3, amp0=args.ampzbl, gain_tol=0.3)
+[obs_cal_avg, master_caltab] = multical(obs_cal_avg, sites, master_caltab, n=2, amp0=args.ampzbl, gain_tol=0.3)
 
 # Recalibrate all redundant stations
 stepname = 'step3'
 sites = {'AA','AP','SM','JC'}
-obs_cal_avg = multical(obs_cal_avg, sites, n=2, amp0=args.ampzbl, gain_tol=0.1)
+[obs_cal_avg, master_caltab] = multical(obs_cal_avg, sites, master_caltab, n=2, amp0=args.ampzbl, gain_tol=0.1)
 
 # Save output
 obs_cal_avg.save_uvfits(args.output)
+master_caltab.save_txt(obs, datadir=os.path.basename(args.input[:-13]) + '/master_caltab')
