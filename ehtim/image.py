72e14f8dbe3a6f349ce1e2ab89b7565fadff06b6
Katherine Bouman <klbouman@Katherines-MacBook-Pro-5.local>
2018-02-07 20:18:44 -0500

    fixed bug in t_int for vex files

--- ehtim/image.py
+++ ehtim/image.py
@@ -406,11 +406,12 @@ class Image(object):
 
         """
 
-        if t_int == 0.0:
-            t_int = vex.sched[i_scan]['scan'][0]['scan_sec']
-
         obs_List=[]
         for i_scan in range(len(vex.sched)):
+        
+            if t_int == 0.0:
+                 t_int = vex.sched[i_scan]['scan'][0]['scan_sec']
+        
             if vex.sched[i_scan]['source'] != source:
                 continue
             subarray = vex.array.make_subarray([vex.sched[i_scan]['scan'][key]['site'] for key in list(vex.sched[i_scan]['scan'].keys())])
