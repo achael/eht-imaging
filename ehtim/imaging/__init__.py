9e7a1ca0c9c29fbdb068bceb039a27820342ad3a
Andrew Chael <achael@cfa.harvard.edu>
2018-02-14 23:06:04 -0500

    rebuilt docs

--- ehtim/imaging/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-.. module:: ehtim.imaging
-    :platform: Unix
-    :synopsis: EHT Imaging Utilities: imaging functions
-
-.. moduleauthor:: Andrew Chael (achael@cfa.harvard.edu)
-
-"""
-from ..const_def import *
