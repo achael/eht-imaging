9262146c115e5f13ef622c448de39dca677c9c19
Maciej Wielgus <maciek.wielgus@gmail.com>
2018-04-11 14:06:21 -0400

    test

--- ehtim/observing/obs_helpers.py
+++ ehtim/observing/obs_helpers.py
@@ -765,3 +765,119 @@ def xyz_2_latlong(obsvecs):
 
     #if out.shape[0]==1: out = out[0]
     return out
+
+def roundTime(dt=None, roundTo=60):
+    from astropy.time import Time
+    import datetime as datetime
+    if dt == None: 
+        dt = datetime.datetime.now()
+    seconds = (dt.replace(tzinfo=None) - dt.min).seconds
+    rounding = (seconds+roundTo/2) // roundTo * roundTo
+    return dt + datetime.timedelta(0,rounding-seconds,-dt.microsecond)
+
+def jd2expt2017(jd):
+    '''
+    Function translating from jd to expt for April 2017 EHT
+    '''
+    if (jd > 2457853.470)&(jd < 2457854.132 ):
+        return 3600
+    elif (jd > 2457849.531)&(jd < 2457850.177):
+        return 3598
+    elif (jd > 2457850.667)&(jd < 2457851.363):
+        return 3599
+    elif (jd > 2457848.438)&(jd < 2457849.214):
+        return 3597
+    elif (jd > 2457854.427)&(jd < 2457855.141):
+        return 3601
+    else:
+        return None
+
+def make_scan_list_EHT2017(fpath):
+    '''
+    generates data frame with information about scans for EHT2017
+    '''
+    import ehtim.vex as vex
+    import os
+    import pandas as pd
+    from astropy.time import Time, TimeDelta
+    import datetime as datetime
+    nam2lett = {'ALMA':'A','APEX':'X','LMT':'L','PICOVEL':'P','SMTO':'Z','SPT':'T','JCMT':'J','SMAP':'S'}
+    track2expt ={'D':3597,'B':3598, 'C':3599,'A':3600,'E':3601}
+    list_files = os.listdir(fpath)
+    scans = pd.DataFrame({'source' : []})
+
+    for fi in list_files:#loop over vex files in folder
+        track_loc = fi[3].upper()
+        vpath = fpath+fi
+        aa = vex.Vex(vpath)
+        dec = []
+        for cou in range(len(aa.source)):
+            dec_h = float(aa.source[cou]['dec'].split('d')[0])
+            dec_m = float((aa.source[cou]['dec'].split('d')[1])[0:2])
+            dec_s = float((aa.source[cou]['dec'].split('d')[1])[3:-1])
+            dec.append(tuple((dec_h,dec_m,dec_s)))    
+        ra = []
+        for cou in range(len(aa.source)):
+            ra_d = float(aa.source[cou]['ra'].split('h')[0])
+            ra_m = float(aa.source[cou]['ra'].split('h')[1].split('m')[0])
+            ra_s = float(aa.source[cou]['ra'].split('h')[1].split('m')[1][:-1])
+            ra.append(tuple((ra_d,ra_m,ra_s)))      
+        sour_name = [aa.source[x]['source'] for x in range(len(aa.source))]
+        dict_ra = dict(zip(sour_name,ra))
+        dict_dec = dict(zip(sour_name,dec))
+        t_min = [aa.sched[x]['start_hr'] for x in range(len(aa.sched))]
+        sour = [aa.sched[x]['source'] for x in range(len(aa.sched))]
+        datet = []
+        elev = []
+        antenas = []
+        duration=[]
+        for x in range(len(aa.sched)):#loop over scans in given file
+            t = Time(aa.sched[x]['mjd_floor'], format='mjd', scale='utc')
+            tiso = Time(t, format='iso', scale='utc')
+            tiso = tiso + TimeDelta(t_min[x]*3600., format='sec')
+            datet.append(tiso)
+            ant_foo = set([nam2lett[aa.sched[x]['scan'][y]['site']] for y in range(len(aa.sched[x]['scan']))])
+            antenas.append(ant_foo)
+            duration_foo =max([aa.sched[x]['scan'][y]['scan_sec'] for y in range(len(aa.sched[x]['scan']))])
+            duration.append(duration_foo)
+        #time_min = [pd.tslib.Timestamp(datet[x].datetime) for x in range(len(datet))]
+        time_min = [pd.Timestamp(datet[x].datetime) for x in range(len(datet))]
+        time_max = [time_min[x] + datetime.timedelta(seconds=duration[x]) for x in range(len(aa.sched))]
+        foo = pd.DataFrame(aa.sched)
+        foo = foo[['source','mjd_floor','start_hr']]
+        foo['time_min']=time_min
+        foo['time_max']=time_max
+        foo['scan_no'] = foo.index
+        foo['scan_no'] = list(map(int,foo['scan_no']))
+        foo['track'] = [track_loc]*foo.shape[0]
+        foo['expt'] = [int(track2expt[track_loc])]*foo.shape[0]
+        foo['antenas'] = antenas
+        foo['duration'] = duration
+        scans = pd.concat([scans,foo], ignore_index=True)
+    scans = scans.reindex_axis(['mjd_floor','expt','track','scan_no','source','time_min','time_max','duration','antenas'],axis=1)
+    scans = scans.sort_values('time_max')
+    scans = scans.reset_index(drop=True)
+    scans['scan_no_tot'] = scans.index
+    return scans
+
+def match_scans(scans,data):
+    '''
+    matches data with scans
+    '''
+    import pandas as pd
+    from astropy.time import Time, TimeDelta
+    import datetime as datetime
+    bins_labels = [None]*(2*scans.shape[0]-1)
+    bins_labels[1::2] = map(lambda x: -x-1,list(scans['scan_no_tot'])[:-1])
+    bins_labels[::2] = list(scans['scan_no_tot'])
+    dtmin = datetime.timedelta(seconds = 2.) 
+    dtmax = datetime.timedelta(seconds = 2.) 
+    binsT = [None]*(2*scans.shape[0])
+    binsT[::2] = list(map(lambda x: x - dtmin,list(scans.time_min)))
+    binsT[1::2] = list(map(lambda x: x + dtmax,list(scans.time_max))) 
+    ordered_labels = pd.cut(data.datetime, binsT,labels = bins_labels)
+    data['scan_no_tot'] = ordered_labels
+    data = data[list(map(lambda x: x >= 0, data['scan_no_tot']))]
+    data['scan_id'] = data['scan_no_tot']
+    data.drop('scan_no_tot',axis=1,inplace=True)
+    return data
