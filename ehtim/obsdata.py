fe6724f03044b0aa9d71544c4f9c3c548cf67c54
klbouman <klbouman@gmail.com>
2018-07-10 17:41:44 -0400

    fixed bug introduced in updating the website

--- ehtim/obsdata.py
+++ ehtim/obsdata.py
@@ -1877,7 +1877,7 @@ class Obsdata(object):
         tri = (site1, site2, site3)
         outdata = []
         for obs in bs:
-            obstri = (obs['t1'][0],obs['t2'][0],obs['t3'][0])
+            obstri = (obs['t1'],obs['t2'],obs['t3'])
             if set(obstri) == set(tri):
                 # Flip the sign of the closure phase if necessary
                 parity = paritycompare(tri, obstri)
@@ -1932,7 +1932,7 @@ class Obsdata(object):
         tri = (site1, site2, site3)
         outdata = []
         for obs in cphases:
-            obstri = (obs['t1'][0],obs['t2'][0],obs['t3'][0])
+            obstri = (obs['t1'],obs['t2'],obs['t3'])
             if set(obstri) == set(tri):
                 # Flip the sign of the closure phase if necessary
                 parity = paritycompare(tri, obstri)
