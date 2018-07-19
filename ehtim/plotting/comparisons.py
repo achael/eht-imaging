7e6b5e0bb7c3865faaee78a4d3fbdc90e47979fa
klbouman <klbouman@gmail.com>
2018-07-19 16:17:43 -0400

    cleaning up

--- ehtim/plotting/comparisons.py
+++ ehtim/plotting/comparisons.py
@@ -21,9 +21,6 @@ from matplotlib.offsetbox import (TextArea, DrawingArea, OffsetImage,
 import glob
 from itertools import cycle
 
-#matplotlib.use('TkAgg')
-
-
 
 def image_consistency(imarr, beamparams, metric='nxcorr', blursmall=True, beam_max=1.0, beam_steps=5, savepath=[]):
     
@@ -126,99 +123,6 @@ def image_agreements(imarr, beamparams, metric_mtx, fracsteps, cutoff=0.95):
 def change_cut_off(metric_mtx, fracsteps, imarr, beamparams, cutoff=0.95, zoom=0.1, fov=1):
     (cliques_fraclevels, im_cliques_fraclevels) = image_agreements(imarr, beamparams, metric_mtx, fracsteps, cutoff=cutoff)
     generate_consistency_plot(cliques_fraclevels, im_cliques_fraclevels, metric_mtx=metric_mtx, fracsteps=fracsteps, beamparams=beamparams, zoom=zoom, fov=fov)
-           
- 
-# def generate_consistency_plot(clique_fraclevels, im_clique_fraclevels, metric_mtx=None, fracsteps=None, beamparams=None, imarr=None, show=True):
-#     # matplotlib aesthetics
-#     plt.rc('text', usetex=True)
-#     plt.rc('font', family='serif')
-#     plt.rcParams.update({'font.size': 16})
-#     plt.rcParams['axes.linewidth'] = 2 #set the value globally
-#     plt.rcParams["font.weight"] = "bold"
-#     # fig, ax = plt.subplots()
-#     fig = Figure(figsize=(5,5), dpi=100)
-#     ax = fig.add_subplot(111)
-#     cycol = cycle('bgrcmk')
- 
-#     for c, column in enumerate(clique_fraclevels):
-#         colorc = cycol.next()
-#         for r, row in enumerate(column):
- 
-#             # adding the images
-#             lenx = len(clique_fraclevels)
-#             leny = 0
-#             for li in clique_fraclevels:
-#                 if len(li) > leny:
-#                     leny = len(li)
-#             sample_image = im_clique_fraclevels[c][r]
-#             arr_img = sample_image.imvec.reshape(sample_image.xdim, sample_image.ydim)
-#             imagebox = OffsetImage(arr_img, zoom=0.1, cmap='afmhot')
-#             imagebox.image.axes = ax
- 
-#             ab = AnnotationBbox(imagebox, ((20./lenx)*c,(20./leny)*r),
-#                                 xycoords='data',
-#                                 pad=0.0,
-#                                 arrowprops=None)
- 
-#             ax.add_artist(ab)
- 
-#             # adding the arrows
-#             if c+1 != len(clique_fraclevels):
-#                 for a, ro in enumerate(clique_fraclevels[c+1]):
-#                     if set(row).issubset(ro):
-#                         px = c+1
-#                         px = ((20./lenx)*px)
-#                         py = a
-#                         py = (20./leny)*py
-#                         break
- 
-#                 xx = (20./lenx)*c + (8./lenx)
-#                 yy = (20./leny)*r
-#                 ax.arrow(   xx, yy,
-#                             px - xx - (9./lenx), py- yy,  
-#                             head_width=0.05,
-#                             head_length=0.1,
-#                             color=colorc
-#                         )
-#             row.sort()
- 
-#             # adding the text
-#             txtstring = str(row)
-#             if len(row) == len(clique_fraclevels[-1][0]):
-#                 txtstring = '[all]'
-#             ax.text((20./lenx)*c - (0./lenx), (20./leny)*r  - (10./leny), txtstring, fontsize=6, horizontalalignment='center')
- 
-#     ax.set_xlim(0, 22)
-#     ax.set_ylim(-1, 22)
- 
-#     for item in [fig, ax]:
-#         item.patch.set_visible(False)
-#     fig.patch.set_visible(False)
-#     ax.axis('off')
- 
-#     if show == True:
-#         # plt.savefig("tmp_consis.png")
-#         # try:
-#         #     main.destroy()
-#         # except:
-#         #     pass
-#         main = Tk()
- 
-#         slider = Scale(main, from_=0, to=100, label='Cutoff (%)')
-#         slider.pack()
-#         change_cutoff_BUTTON = Button(main, text='Rerender graph', command=lambda:change_cut_off(metric_mtx, fracsteps, imarr, beamparams, cutoff=int(slider.get())))
-#         change_cutoff_BUTTON.pack()
- 
-#         # plt.show()
-#         canvas = FigureCanvasTkAgg(fig)
-#         # canvas.show()
-#         canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
- 
-#         # toolbar = NavigationToolbar2TkAgg(canvas)
-#         # toolbar.update()
-#         canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
- 
-#         main.mainloop()
            
  
 def generate_consistency_plot(clique_fraclevels, im_clique_fraclevels, zoom=0.1, fov=1, show=True, framesize=(20,10)):
