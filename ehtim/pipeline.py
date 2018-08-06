6188b02c9e866bd03a2898d4a0fe150197d0f002
Chi-kwan Chan <chanc@email.arizona.edu>
2018-08-05 23:11:48 -0400

    Put all the pipeline functions into a class

--- ehtim/pipeline.py
+++ ehtim/pipeline.py
@@ -7,58 +7,59 @@
 import ehtim as eh
 import yaml
 
-# Implement individual processes in an imaging pipeline
-def load(name):
-    return eh.obsdata.load_uvfits(name)
+class Pipeline:
+    # Implement individual processes in an imaging pipeline
+    def load(name):
+        return eh.obsdata.load_uvfits(name)
 
-def scale(obs,
-          zbl=None,   uvdist=1.0e9,
-          noise=None, max_diff_sec=100.0):
-    if zbl is not None:
-        d = obs.data
-        b = d['u']**2 + d['v']**2 < uvdist**2
-        for t in ["vis", "sigma"]:
-            for p in ["", "q", "u", "v"]:
-                d[p+t][b] *= zbl
-        return obs
-    if noise is not None:
-        if noise == 'auto':
-            noise = obs.estimate_noise_rescale_factor(max_diff_sec=max_diff_sec)
-        return obs.rescale_noise(noise_rescale_factor=noise)
+    def scale(obs,
+              zbl=None,   uvdist=1.0e9,
+              noise=None, max_diff_sec=100.0):
+        if zbl is not None:
+            d = obs.data
+            b = d['u']**2 + d['v']**2 < uvdist**2
+            for t in ["vis", "sigma"]:
+                for p in ["", "q", "u", "v"]:
+                    d[p+t][b] *= zbl
+            return obs
+        if noise is not None:
+            if noise == 'auto':
+                noise = obs.estimate_noise_rescale_factor(max_diff_sec=max_diff_sec)
+            return obs.rescale_noise(noise_rescale_factor=noise)
 
-def average(obs, sec=300, old=False):
-    if old:
-        print("WARNING: using old coherent average method")
-        return obs.avg_coherent_old(sec)
-    else:
-        return obs.avg_coherent(sec)
+    def average(obs, sec=300, old=False):
+        if old:
+            print("WARNING: using old coherent average method")
+            return obs.avg_coherent_old(sec)
+        else:
+            return obs.avg_coherent(sec)
 
-def flag(obs,
-         anomalous=None, max_diff_sec=300,
-         low_snr=None,
-         uv_min=None):
-    if anomalous is not None:
-        return obs.flag_anomalous(field=anomalous,
-                                  max_diff_seconds=max_diff_sec)
-    if low_snr is not None:
-        return obs.flag_low_snr(low_snr)
-    if uv_min is not None:
-        return obs.flag_uvdist(uv_mind)
+    def flag(obs,
+             anomalous=None, max_diff_sec=300,
+             low_snr=None,
+             uv_min=None):
+        if anomalous is not None:
+            return obs.flag_anomalous(field=anomalous,
+                                      max_diff_seconds=max_diff_sec)
+        if low_snr is not None:
+            return obs.flag_low_snr(low_snr)
+        if uv_min is not None:
+            return obs.flag_uvdist(uv_mind)
 
-def merge(obss):
-    for i in range(1, obss):
-        obss[i].data['time'] += i * 1e-6
-        obss[i].mjd = obss[0].mjd
-        obss[i].rf  = obss[0].rf
-        obss[i].ra  = obss[0].ra
-        obss[i].dec = obss[0].dec
-        obss[i].bw  = obss[0].bw
-    return eh.obsdata.merge_obs(obss).copy()
+    def merge(obss):
+        for i in range(1, obss):
+            obss[i].data['time'] += i * 1e-6
+            obss[i].mjd = obss[0].mjd
+            obss[i].rf  = obss[0].rf
+            obss[i].ra  = obss[0].ra
+            obss[i].dec = obss[0].dec
+            obss[i].bw  = obss[0].bw
+        return eh.obsdata.merge_obs(obss).copy()
 
-def reorder(obs, according='snr'):
-    if according == 'snr':
-        obs.reorder_tarr_snr()
-        return obs
+    def reorder(obs, according='snr'):
+        if according == 'snr':
+            obs.reorder_tarr_snr()
+            return obs
 
 # Factory method
 def make_process(func_name, **kwargs):
