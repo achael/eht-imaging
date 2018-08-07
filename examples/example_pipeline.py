029686d20387b673cb085fc54e7ef9701ede64c3
Chi-kwan Chan <chanc@email.arizona.edu>
2018-08-07 00:10:06 -0400

    Print additional comments in the example script

--- examples/example_pipeline.py
+++ examples/example_pipeline.py
@@ -3,12 +3,18 @@
 import ehtim as eh
 import yaml
 
+print("----------------------------------------------------------------")
+print("Use the ehtim pipeline interface as a pipeline constructor")
+
 with open("example_pipeline.yaml", 'r') as f:
     dict = yaml.load(f)
 
 obs = eh.Pipeline(dict) \
     .apply(["M87/er4v2/data/lo/hops_3601_M87.LL+netcal.uvfits",
             "M87/er4v2/data/lo/hops_3601_M87.RR+netcal.uvfits"])
+
+print("----------------------------------------------------------------")
+print("Use the ehtim pipeline interface with method chaining notations")
 
 obs = eh.Pipeline(["M87/er4v2/data/lo/hops_3601_M87.LL+netcal.uvfits",
                    "M87/er4v2/data/lo/hops_3601_M87.RR+netcal.uvfits"]) \
