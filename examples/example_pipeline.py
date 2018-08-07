2d695da2ca7611ea4a940e9610e1b8aee2de8c38
Chi-kwan Chan <chanc@email.arizona.edu>
2018-08-07 03:10:32 -0400

    Clean up the example

--- examples/example_pipeline.py
+++ examples/example_pipeline.py
@@ -3,21 +3,23 @@
 import ehtim as eh
 import ruamel.yaml as yaml
 
+input = "M87/er4v2/data/lo/hops_3601_M87.LL+netcal.uvfits"
+
 print("----------------------------------------------------------------")
 print("Use the ehtim pipeline interface as a pipeline constructor")
 
 with open("example_pipeline.yaml", 'r') as f:
-    dict = yaml.load(f)
+    pipeline = eh.Pipeline(yaml.load(f))
 
-pipeline = eh.Pipeline(dict)
-obs = pipeline.apply("M87/er4v2/data/lo/hops_3601_M87.LL+netcal.uvfits")
+obs = pipeline.apply(input).data
 
 print("----------------------------------------------------------------")
-print("Use the ehtim pipeline interface with method chaining notations")
+print("Use the ehtim pipeline interface with method-chaining notation")
 
-obs = eh.Pipeline("M87/er4v2/data/lo/hops_3601_M87.LL+netcal.uvfits") \
+obs = eh.Pipeline(input) \
     .load() \
     .average(minlen=500, sec=300.0) \
     .flag(uv_min=0.1e9) \
     .flag(site='SR') \
-    .reorder()
+    .reorder() \
+    .data
