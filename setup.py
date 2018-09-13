91aafb1c41a416266394b77b99181cbcf76e742d
Chi-kwan Chan <chanc@email.arizona.edu>
2018-09-12 20:59:47 -0700

    Clean up "setup.py"; add all the required packages

--- setup.py
+++ setup.py
@@ -5,18 +5,37 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 if __name__ == "__main__":
-   setup(
-        name="ehtim",
-        version = "1.0.0",
+    setup(name="ehtim",
+          version = "1.0.0",
 
-        author = "Andrew Chael",
-        author_email = "achael@cfa.harvard.edu",
-        description = ("Python code to load, simulate, and manipulate VLBI datasets" +
-                       "and a collection of imaging functions in total intensity and polarization."),
-        license = "GPLv3",
-        keywords = "imaging astronomy EHT polarimetry",
-        url = "https://github.com/achael/eht-imaging",
-        packages = ["ehtim","ehtim.calibrating","ehtim.plotting","ehtim.imaging","ehtim.observing","ehtim.io","ehtim.scattering","ehtim.statistics"],
-        long_description=read('README.rst'),
-        install_requires=["astropy", "ephem", "future", "matplotlib", "numpy", "scipy","pandas","requests","scikit-image"]#,"pynfft"],
-        )
+          author = "Andrew Chael",
+          author_email = "achael@cfa.harvard.edu",
+          description = ("Python code to load, simulate, and manipulate VLBI "+
+                         "datasets and a collection of imaging functions in "+
+                         "total intensity and polarization."),
+          license = "GPLv3",
+          keywords = "imaging astronomy EHT polarimetry",
+          url = "https://github.com/achael/eht-imaging",
+          packages = ["ehtim",
+                      "ehtim.calibrating",
+                      "ehtim.imaging",
+                      "ehtim.io",
+                      "ehtim.observing",
+                      "ehtim.plotting",
+                      "ehtim.scattering",
+                      "ehtim.statistics"],
+          long_description=read('README.rst'),
+          install_requires=["astropy",
+                            "ephem",
+                            "future",
+                            "html",
+                            "matplotlib",
+                            "networkx",
+                            "numpy",
+                            "pandas",
+                            "pyfits",
+                            "pynfft",
+                            "requests",
+                            "scipy",
+                            "skimage"]
+    )
