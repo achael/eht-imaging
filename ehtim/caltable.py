e503dfc4e8530c3b8e06483a0cd04504d4a24d2e
klbouman <klbouman@gmail.com>
2018-11-13 07:15:01 -0500

    added plotting of dterms from caltable

--- ehtim/caltable.py
+++ ehtim/caltable.py
@@ -113,6 +113,72 @@ class Caltable(object):
         new_caltable = Caltable(self.ra, self.dec, self.rf, self.bw, self.data, self.tarr, source=self.source, mjd=self.mjd, timetype=self.timetype)
         return new_caltable
 
+    def plot_dterms(self, sites, label=None, legend=True, clist=SCOLORS,rangex=False,
+                    rangey=False, markersize=2*MARKERSIZE, show=True, grid=True, 
+                    axislabels=True, export_pdf=""):
+
+        colors = iter(clist)
+        
+        fig, axes = plt.subplots(nrows=1, ncols=2, sharey=True, sharex=True, figsize=(16,8))
+
+        # sites
+        if sites in ['all' or 'All'] or sites==[]:
+            sites = list(self.data.keys())
+
+        if not type(sites) is list:
+            sites = [sites]
+        
+        keys = [self.tkey[site] for site in sites]
+            
+        for key in keys:
+            
+            # get the label
+            site = str(self.tarr[key]['site'])
+            if label is None:
+                bllabel=str(site)
+            else:
+                bllabel = label + ' ' + str(site)
+            color = next(colors)
+
+            axes[0].plot(np.real(self.tarr[key]['dr']), np.imag(self.tarr[key]['dr']), color=color, marker='o', markersize=markersize, 
+                     label=bllabel, linestyle='none')
+            axes[0].set_title("Right D-terms")
+            axes[0].set_xlabel("Real")
+            axes[0].set_ylabel("Imaginary");
+                  
+            axes[1].plot(np.real(self.tarr[key]['dl']), np.imag(self.tarr[key]['dl']), color=color, marker='o', markersize=markersize, 
+                     label=bllabel, linestyle='none')
+            axes[1].set_title("Left D-terms")
+            axes[1].set_xlabel("Real")
+            axes[1].set_ylabel("Imaginary");
+        
+        axes[0].axhline(y=0, color='k')
+        axes[0].axvline(x=0, color='k')
+        axes[1].axhline(y=0, color='k')
+        axes[1].axvline(x=0, color='k')
+            
+        if grid: 
+            axes[0].grid()
+            axes[1].grid()
+            
+        if rangex: 
+            axes[0].set_xlim(rangex)
+            axes[1].set_xlim(rangex)
+            
+        if rangey: 
+            axes[0].set_ylim(rangey)
+            axes[1].set_ylim(rangey)
+        
+        if legend:
+            #axes[0].legend()
+            axes[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+        if export_pdf != "":
+            fig.savefig(export_pdf, bbox_inches='tight')
+        
+        return axes
+
+
     def plot_gains(self, sites, gain_type='amp', pol='R',label=None,
                    ang_unit='deg',timetype=False, yscale='log', legend=True,
                    clist=SCOLORS,rangex=False,rangey=False, markersize=MARKERSIZE,
