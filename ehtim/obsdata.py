655ab81121c3fbbaaf4800328a907d7b8de86085
Andrew Chael <achael@princeton.edu>
2022-05-04 22:57:36 -0400

    fixed bug in unpack_bl

--- ehtim/obsdata.py
+++ ehtim/obsdata.py
@@ -598,8 +598,8 @@ class Obsdata(object):
 
                     allout.append(out)
 
-#        return np.array(datalist, dtype=object)
-        return np.array(datalist)
+#        return np.array(allout, dtype=object)
+        return np.array(allout)
 
     def unpack(self, fields, mode='all', ang_unit='deg', debias=False, conj=False, timetype=False):
         """Unpack the data for the whole observation .
