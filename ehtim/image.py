fixed camp gradient bug

ehtim/image.py
@@ -142,8 +142,8 @@ class Image(object):
         bvec = np.array([])
         avec_pol = np.array([]) 
         bvec_pol = np.array([])
-        self.rmvec = np.array([])
-        self.cmvec = np.array([])        
+        rmvec = np.array([])
+        cmvec = np.array([])        
         self._mflist = [avec, bvec, avec_pol, bvec_pol, rmvec, cmvec]
 
         # Save the image dimension data
@@ -448,7 +448,6 @@ class Image(object):
         """Return the fractional polarization angle for each pixel"""
 
         return self.chivec
-
 
     @property
     def rhovec(self):

ehtim/imaging/imager_utils.py
@@ -624,11 +624,12 @@ def chisqgrad_camp(imvec, Amatrices, clamp, sigma):
     pt2 = pp/i2
     pt3 = -pp/i3
     pt4 = -pp/i4
+            
     out = (np.dot(pt1, Amatrices[0]) +
            np.dot(pt2, Amatrices[1]) +
            np.dot(pt3, Amatrices[2]) +
            np.dot(pt4, Amatrices[3]))
-    out *= (-2.0/len(clamp)) * np.real(out)
+    out = (-2.0/len(clamp)) * np.real(out)
     return out
 
 