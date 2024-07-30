fixed egregious bug in multifrequency gradients

ehtim/image.py
@@ -819,12 +819,13 @@ class Image(object):
             if mfvec.size:
                 log_imvec += mfvec * (log_nufrac**(n + 1))
         imvec = np.exp(log_imvec)
-
+        
         arglist, argdict = self.image_args()
         arglist[0] = imvec.reshape(self.ydim, self.xdim)
         argdict['rf'] = nu
         outim = Image(*arglist, **argdict)
 
+        
         # Polarization
         do_mf_pol=False
         for n in range(4):
@@ -878,8 +879,8 @@ class Image(object):
             outim.add_pol_image(qimage_out.reshape(outim.ydim, outim.xdim), 'Q')  
             outim.add_pol_image(uimage_out.reshape(outim.ydim, outim.xdim), 'U')            
             outim.add_pol_image(vimage_out.reshape(outim.ydim, outim.xdim), 'V') 
-                                                                 
-            outim = im_stokes.switch_polrep(polrep_out=self.polrep)           
+            outim = outim.switch_polrep(polrep_out=self.polrep)           
+
         else:
             outim.copy_pol_images(self)
 

ehtim/imager.py
@@ -665,7 +665,8 @@ class Imager(object):
         if (self.prior_next.polrep == 'circ' and not(self.pol_next in ['RR', 'LL'])):
             raise Exception("Initial image polrep is 'circ': pol_next must be 'RR' or 'LL'")
 
-        if (self.prior_next.polrep == 'stokes' and not(self.pol_next in ['I', 'Q', 'U', 'V', 'P','IP','IQU','IV','IQUV'])):
+        if (self.prior_next.polrep == 'stokes' 
+            and not(self.pol_next in ['I', 'Q', 'U', 'V', 'P','IP','IQU','IV','IQUV'])):
             raise Exception(
                 "Initial image polrep is 'stokes': pol_next must be in 'I', 'Q', 'U', 'V', 'P','IP','IQU','IV','IQUV'!")
 
@@ -1250,7 +1251,10 @@ class Imager(object):
 
                     # If imaging Stokes I with polarization simultaneously, bundle the gradient
                     if self.pol_next in POLARIZATION_MODES: 
-                        chi2grad = np.array((chi2grad,np.zeros(self._nimage),np.zeros(self._nimage),np.zeros(self._nimage)))
+                        chi2grad = np.array((chi2grad,
+                                             np.zeros(self._nimage),
+                                             np.zeros(self._nimage),
+                                             np.zeros(self._nimage)))
 
                 else:
                     raise Exception("data term %s not recognized!" % dname)
@@ -1410,7 +1414,7 @@ class Imager(object):
                         idx = 8
                     elif regname in REGULARIZERS_CM:
                         idx = 9
-                    
+
                     regmf = mfutils.regularizergrad_mf(imcur[idx], self._xprior[idx], self._embed_mask,
                                                        self.prior_next.xdim, self.prior_next.ydim, self.prior_next.psize,
                                                        regname,
@@ -1803,6 +1807,7 @@ def unpack_imarr(vec, priorarr, which_solve):
     """unpack minimized vector vec into array,
        replace quantities not solved for with their initial values
     """
+    
     imarrdim = len(priorarr.shape)
     if imarrdim==2:
         nsolve = priorarr.shape[0]    
@@ -1900,7 +1905,7 @@ def transform_imarr_inverse(imarr, transforms, which_solve):
     if nimage==1 and ('log' in transforms):
         outarr = np.log(imarr)         
     elif nimage==3 and ('log' in transforms):
-        outarr[0] = np.exp(outarr[0])           
+        outarr[0] = np.log(outarr[0])           
     else:
 
         if pol_which_solve[0]==1 and ('log' in transforms):  # full polarization, including stokes I imaging

ehtim/imaging/multifreq_imager_utils.py
@@ -63,6 +63,7 @@ def image_at_freq(mfarr, log_freqratio):
             logimvec = np.log(imvec0) + alpha*log_freqratio + beta*log_freqratio*log_freqratio
             imvec = np.exp(logimvec)
             out = imvec
+
             
         # Full Polarization    
         elif len(mfarr==10):
@@ -121,7 +122,7 @@ def mf_all_grads_chain(funcgrad, image_cur, mfarr, log_freqratio):
             dfunc_dbeta  = funcgrad * imvec_cur * log_freqratio * log_freqratio
 
             out = np.array((dfunc_dI0, dfunc_dalpha, dfunc_dbeta))
-            
+
         # Full Polarization    
         elif len(mfarr==10):
             # current image
@@ -199,7 +200,7 @@ def regularizergrad_mf(imvec, nprior, mask, xdim, ydim, psize, stype, **kwargs):
     beam_size = kwargs.get('beam_size', psize)
 
     if "l2_" in stype:
-        s = -l2_spec_grad(imvec, nprior)
+        s = -l2_spec_grad(imvec, nprior, norm_reg=norm_reg)
     elif "tv_" in stype:
         if np.any(np.invert(mask)):
             imvec = embed(imvec, mask, clipfloor=0, randomfloor=False)
@@ -231,8 +232,9 @@ def l2_spec_grad(imvec, priorvec, norm_reg=NORM_REGULARIZER):
         norm = float(len(imvec))
     else:
         norm = 1
-        
-    out = -2*(np.sum(imvec - priorvec))*np.ones(len(imvec))
+    out = -2*(imvec - priorvec)
+    # PIN TODO HOW WAS THIS WRONG???????????
+#    out = -2*(np.sum(imvec - priorvec))*np.ones(len(imvec))
     return out/norm
 
 