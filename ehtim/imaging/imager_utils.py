2ffb4b59592b60949049305d520e5f4154c86c17
Andrew Chael <achael@princeton.edu>
2024-07-29 20:17:53 -0400

    fix bug in chisqgrad_camp in imager_utils.py

--- ehtim/imaging/imager_utils.py
+++ ehtim/imaging/imager_utils.py
@@ -944,7 +944,7 @@ def chisqgrad_camp(imvec, Amatrices, clamp, sigma):
            np.dot(pt2, Amatrices[1]) +
            np.dot(pt3, Amatrices[2]) +
            np.dot(pt4, Amatrices[3]))
-    out *= (-2.0/len(clamp)) * np.real(out)
+    out = (-2.0/len(clamp)) * np.real(out)
     return out
 
 
