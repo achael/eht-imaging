572cd29e9e41622796cbaddff7f7151e12d3a360
Andrew Chael <achael@princeton.edu>
2026-05-13 17:45:33 +0200

    rebuilt docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.11',
+    VERSION: '1.3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
